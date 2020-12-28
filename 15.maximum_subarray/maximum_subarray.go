3a648b8e63917f9084f929dc951c1e8138b95fe1
vutuong <tuongvu180295@gmail.com>
2020-12-28 08:42:29 +0000

    Add problem explain Maximum subarray

--- 15.maximum_subarray/maximum_subarray.go
+++ 15.maximum_subarray/maximum_subarray.go
@@ -2,6 +2,30 @@ package main
 
 import "fmt"
 
+// 53. Maximum Subarray
+// Easy
+
+// Share
+// Given an integer array nums, find the contiguous subarray (containing at least one number)
+// which has the largest sum and return its sum.
+// Follow up: If you have figured out the O(n) solution,
+// try coding another solution using the divide and conquer approach, which is more subtle.
+
+// Example 1:
+
+// Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+// Output: 6
+// Explanation: [4,-1,2,1] has the largest sum = 6.
+// Example 2:
+
+// Input: nums = [1]
+// Output: 1
+// Example 3:
+
+// Input: nums = [0]
+// Output: 0
+// Example 4:
+
 func maxSubArray(nums []int) int {
 	maxResult := nums[0]
 	maxTemporary := 0
