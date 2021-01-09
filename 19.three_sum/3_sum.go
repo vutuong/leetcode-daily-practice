a76a12d96f59e575ba54d2d8261d43f89e8fc076
vutuong <tuongvu180295@gmail.com>
2021-01-09 10:15:42 +0000

    add problem explain

--- 19.three_sum/3_sum.go
+++ 19.three_sum/3_sum.go
@@ -6,6 +6,18 @@ import (
 	"sort"
 )
 
+/* 15. 3Sum
+
+Share
+Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0?
+Find all unique triplets in the array which gives the sum of zero.
+Notice that the solution set must not contain duplicate triplets.
+
+Example 1:
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+*/
+
 // Initial solution: Brute-force O( n³ ) nested loop scan
 func compareSlice(s1, s2 []int) bool {
 	sort.Ints(s1)
